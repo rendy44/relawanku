18ff7e7704373d2f3db97f11172442e1fcb05e30
rendi <deQueen44>
2021-08-03 21:50:11 +0700

    Adding simple template for volunteer details

--- single-volunteer.php
+++ single-volunteer.php
@@ -19,11 +19,32 @@ get_header();
 while ( have_posts() ) {
 	the_post();
 
+	// Get basic information of the volunteer.
 	$volunteer_id = get_the_ID();
-	$page_title = get_the_title();
-	$hero_small = true;
-	$skills = $helper->get_post_meta($volunteer_id,'skill');
-	Template::init()->render( 'single-volunteer', compact( 'page_title', 'hero_small' ) );
+	$page_title   = get_the_title();
+	$hero_small   = true;
+	$details      = array(
+		'name' => array( __( 'Full name', 'relawanku' ), $page_title ),
+	);
+	// Get more details of the volunteer.
+	$id_card    = $helper->get_post_meta( $volunteer_id, 'id_card_number' );
+	$phone      = $helper->get_post_meta( $volunteer_id, 'phone' );
+	$valid_from = $helper->get_post_meta( $volunteer_id, 'valid_from' );
+	$valid_from = $valid_from ? $helper->timestamp_to_date( $valid_from ) : false;
+	$valid_to   = $helper->get_post_meta( $volunteer_id, 'valid_to' );
+	$valid_to   = $valid_to ? $helper->timestamp_to_date( $valid_to ) : false;
+	// Make sure only add the details when it is available.
+	if ( $id_card ) {
+		$details['id_card'] = array( __( 'Id card', 'relawanku' ), $id_card );
+	}
+	if ( $phone ) {
+		$details['phone'] = array( __( 'Phone', 'relawanku' ), $phone );
+	}
+	if ( $valid_from && $valid_to ) {
+		$details['validity'] = array( __( 'Valid date', 'relawanku' ), "{$valid_from} to {$valid_to}" );
+	}
+	// Proceed to render the template.
+	Template::init()->render( 'single-volunteer', compact( 'page_title', 'hero_small', 'details' ) );
 }
 
 get_footer();
--- templates/single-volunteer.blade.php
+++ templates/single-volunteer.blade.php
@@ -1,1 +1,20 @@
 @includeIf('parts.hero')
+<section class="relawanku-section section-volunteer">
+    <div class="frow-container">
+        <div class="frow">
+            <div class="col-md-2-3">
+                <div class="section-wrapper">
+                    <h2>Details</h2>
+                    <div class="details-row">
+                        @foreach($details as $detailId => $detailArr)
+                            <div class="detail-item">
+                                <label for="{{$detailId}}">{{$detailArr[0]}}</label>
+                                <span id="{{$detailId}}">{{$detailArr[1]}}</span>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
